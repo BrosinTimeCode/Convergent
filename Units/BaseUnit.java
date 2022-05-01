3d7bad3f6e7e64c7ff62499d703776789fd6174b
Nurdoidz <nurdoidz@live.com>
2022-05-01 07:40:03 -0600

    Added basic Tradesman unit

---
+++ Units/BaseUnit.java
@@ -0,0 +1,17 @@
+package Units;
+
+public class BaseUnit {
+    public char name;
+    public int speed;
+    public int health;
+    public int rangeSight;
+    public int rangeAttack;
+    public int armorMelee;
+    public int armorHeavy;
+    public int armorMagic;
+    public int armorGun;
+    public int attackMelee;
+    public int attackHeavy;
+    public int attackMagic;
+    public int attackGun;
+}
---
+++ Units/Tradesman.java
@@ -0,0 +1,19 @@
+package Units;
+
+public class Tradesman extends BaseUnit {
+    public Tradesman() {
+        name = 'T';
+        speed = 1;
+        health = 100;
+        rangeSight = 3;
+        rangeAttack = 1;
+        armorMelee = 5;
+        armorHeavy = 7;
+        armorMagic = 3;
+        armorGun = 1;
+        attackMelee = 6;
+        attackHeavy = 1;
+        attackMagic = 1;
+        attackGun = 4;
+    }
+}
