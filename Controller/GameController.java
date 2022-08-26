5aed4793cb26d08f9bef341234d522c9c8ade000
geektron <jshmunoz@gmail.com>
2022-08-25 18:29:40 -0600

    changed tab sizes on style sheet and added pom.xml again

--- Controller/GameController.java
+++ Controller/GameController.java
@@ -9,30 +9,39 @@ import java.util.Scanner;
 import java.util.Timer;
 
 public class GameController {
+    GameViewInterface viewInterface;
+    BaseBoard board;
 
-  GameViewInterface viewInterface;
-  BaseBoard board;
-
-  public GameController(int viewType, int boardType) {
-    switch (boardType) {
-      case 1:
-        board = new TestBoard();
-        break;
-      default:
-        board = new Board(10, 10);
+    public GameController(int viewType, int boardType) {
+        switch(boardType) {
+            case 1:
+                board = new TestBoard();
+                break;
+            default:
+                board = new Board();
+        }
+        switch(viewType) {
+            default:
+                viewInterface = new CommandLineInterface();
+        }
     }
-    switch (viewType) {
-      default:
-        viewInterface = new CommandLineInterface();
+    public void initialize() {
+        Timer timer = new Timer();
+        long oneSecond = 1000;
+        RefreshMapTask task = new RefreshMapTask(viewInterface, board);
+        timer.schedule(task, 0, oneSecond);
     }
-  }
-
-  public void initialize() {
-    Timer timer = new Timer();
-    long oneSecond = 1000;
-    RefreshMapTask task = new RefreshMapTask(viewInterface, board);
-    timer.schedule(task, 0, oneSecond);
-  }
+    public void handleUserInput() {
+        viewInterface.displayHelp();
+        boolean consoleIsOpen = true;
+        Scanner userInput = new Scanner(System.in);
+        while (consoleIsOpen) {
+            BaseCommand userCommand = Parser.getCommand(userInput.nextLine());
+            if (userCommand == null) {
+                viewInterface.displayInvalidCommand();
+            }
+        }
+    }
     public void handleUserInput() {
         viewInterface.displayHelp();
         boolean consoleIsOpen = true;
