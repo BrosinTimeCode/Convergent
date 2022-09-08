4bbd2cf594477a92a917697479631dd1fbd36721
Nurdoidz <nurdoidz@live.com>
2022-09-08 09:28:23 -0600

    Created tests for CommandList

--- Test/Commands/CommandListTests.java
+++ Test/Commands/CommandListTests.java
@@ -1,6 +1,7 @@
 package Test.Commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import Commands.Command;
 import Commands.CommandList;
@@ -10,13 +11,21 @@ import org.junit.jupiter.api.Test;
 public class CommandListTests {
 
     @Test
-    void aliasesGetAppropriateCommand() {
+    void commandsInCommandList_aliasesGetAppropriateCommand() {
         CommandList.initializeCommands();
         for (Command command : CommandList.COMMANDS) {
             List<String> aliases = command.getAliases();
             for (String alias : aliases) {
                 assertEquals(command, CommandList.getCommandFromAlias(alias));
             }
+        }
+    }
+
+    @Test
+    void commandAliasesAsUserInput_returnsCommand() {
+        for (String aliasAsInput : CommandList.ALIASES.keySet()) {
+            Command command = CommandList.getCommandFromInput(aliasAsInput);
+            assertNotNull(command);
         }
     }
 }
