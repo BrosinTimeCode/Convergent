8ac6a4404ac6c1a237254a29ac002827766b66c7
Nurdoidz <nurdoidz@live.com>
2022-09-15 10:24:52 -0600

    Added documentation to UserInputHistory

--- Controller/UserInputHistory.java
+++ Controller/UserInputHistory.java
@@ -3,35 +3,61 @@ package Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * UserInputHistory is the class used to store and access a chronological history of Strings from
+ * user input. Strings are maintained in the order they were added, with duplicates moving to the
+ * most recent position. Each object instantiated contains its own archive and a Browser, which is a
+ * cursor that moves forward (method: next) and backward (method: previous) through the archive.
+ */
 public class UserInputHistory {
 
     private final List<String> history = new ArrayList<>();
-    private int index = 0;
+    private int browserIndex = 0;
 
+    /**
+     * Adds a String record to the list at index 0 and sets the Browser index to 0. If the record
+     * already exists in the list, the record is instead moved to index 0.
+     *
+     * @param record A String that should be the most recent user input.
+     */
     public void add(String record) {
         this.history.remove(record);
         this.history.add(0, record);
-        this.index = 0;
+        this.browserIndex = 0;
     }
 
+    /**
+     * Gets the record from the list previous from the Browser chronologically and moves the Browser
+     * backward by one. If the browser is at the first record, the first record is returned and the
+     * Browser does not move. If the list is empty, an empty String is returned.
+     *
+     * @return The previous String record from the list chronologically, or empty if nonexistent.
+     */
     public String previous() {
         if (this.history.size() == 0) {
             return "";
         }
-        if (this.index < this.history.size()) {
-            this.index++;
+        if (this.browserIndex < this.history.size()) {
+            this.browserIndex++;
         }
-        return this.history.get(this.index - 1);
+        return this.history.get(this.browserIndex - 1);
     }
 
+    /**
+     * Gets the record from the list next from the Browser chronologically and moves the Browser
+     * forward by one. If the browser is at the last record or the list is empty, an empty String is
+     * returned and the Browser does not move.
+     *
+     * @return The next String record from the list chronologically, or empty if nonexistent.
+     */
     public String next() {
-        if (this.index > 0) {
-            this.index--;
+        if (this.browserIndex > 0) {
+            this.browserIndex--;
         }
-        if (this.index == 0) {
+        if (this.browserIndex == 0) {
             return "";
         }
-        return this.history.get(this.index - 1);
+        return this.history.get(this.browserIndex - 1);
     }
 
     public int size() {
