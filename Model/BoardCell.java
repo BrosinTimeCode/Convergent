17e4fe0a6bdc49228b6455bab6d6a8cb4f6ad4dd
geektron <jshmunoz@gmail.com>
2022-09-17 15:14:14 -0600

    Added methods and data structure for boardcell to have multiple units

--- Model/BoardCell.java
+++ Model/BoardCell.java
@@ -1,17 +1,26 @@
 package Model;
 
 import Units.*;
+import java.util.HashMap;
 
 public class BoardCell {
 
     public BaseUnit unit;
+    HashMap<Integer, BaseUnit> units;
 
     public BoardCell(BaseUnit unit) {
-        this.unit = unit;
+        units = new HashMap<>();
+        if(unit != null) {
+            units.put(unit.getId(), unit);
+        }
     }
 
     public void emptyCell() {
         this.unit = null;
+    }
+
+    public void removeUnit(int id) {
+        units.remove(id);
     }
 
     public boolean setUnit(BaseUnit unit) {
@@ -20,5 +29,9 @@ public class BoardCell {
             return true;
         }
         return false;
+    }
+
+    public void addUnit(BaseUnit unit) {
+        units.put(unit.getId(), unit);
     }
 }
