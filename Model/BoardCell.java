4b2b5ec939582624f741a1aac1e7f913bf3d90d7
geektron <jshmunoz@gmail.com>
2022-09-18 16:19:36 -0600

    Added Javadoc to BoardCell

--- Model/BoardCell.java
+++ Model/BoardCell.java
@@ -15,6 +15,10 @@ public class BoardCell {
         }
     }
 
+    /**
+     * Removes unit in BoardCell with specified id. If BoardCell is not empty visible unit will be selected from other units in BoardCell.
+     * @param id An integer representing the unit to be removed.
+     */
     public void removeUnit(int id) {
         // Current unit is visible unit
         if(units.get(id) == unit) {
@@ -30,11 +34,20 @@ public class BoardCell {
         }
     }
 
+    /**
+     * Adds a unit to BoardCell. Added unit becomes visible unit in BoardCell.
+     * @param unit BaseUnit to be added to BoardCell.
+     */
     public void addUnit(BaseUnit unit) {
         units.put(unit.getId(), unit);
         this.unit = unit;
     }
 
+    /**
+     * Checks if BoardCell has unit identified by id.
+     * @param id An integer representing the id of the unit to search for.
+     * @return A boolean showing if the BoardCell contains unit.
+     */
     public boolean containsUnit(int id) {
         return units.containsKey(id);
     }
