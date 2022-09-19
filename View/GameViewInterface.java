408df53284f74863ce1ce40060c58841afc9d20a
geektron <jshmunoz@gmail.com>
2022-09-18 18:35:04 -0600

    Added GameViewInterface javadoc

--- View/GameViewInterface.java
+++ View/GameViewInterface.java
@@ -3,6 +3,10 @@ package View;
 import Model.Board;
 import com.googlecode.lanterna.input.KeyStroke;
 
+/**
+ * The GameViewInterface interface follows the view design in the MVC design pattern. This class
+ * handles userinput and sends information and commands to the controller.
+ */
 public interface GameViewInterface {
 
     // Method to display the current state of the board
