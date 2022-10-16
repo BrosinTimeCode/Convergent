b6d93ec69ddbe35e29a3bf832fe20293d90013d2
Nurdoidz <nurdoidz@live.com>
2022-10-16 12:20:42 -0600

    Implement basic LibGDX game
    Later on, we may integrate the new GUI into our existing GameView
    interface. For now, I wanted to make sure our Maven checks on Github
    process successfully with the new LibGDX dependencies.
    
    The default game mode has been set to GUI, but the old terminal view
    can still be accessed by passing “nogui” as the first argument to Main.

--- src/main/java/com/brosintime/rts/Main.java
+++ src/main/java/com/brosintime/rts/Main.java
@@ -1,12 +1,29 @@
 package com.brosintime.rts;
 
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.brosintime.rts.Controller.GameController;
+import com.brosintime.rts.View.GuiClient;
 
 public class Main {
 
     public static void main(String[] args) {
-        GameController controller = new GameController();
-        controller.run();
-        System.exit(0);
+
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setIdleFPS(60);
+        config.useVsync(true);
+        config.setTitle("Convergent");
+
+        config.setWindowedMode(1280, 720);
+
+        if (args.length != 0) {
+            if (args[0].equals("nogui")) {
+                GameController controller = new GameController();
+                controller.run();
+                System.exit(0);
+            }
+        }
+        new Lwjgl3Application(new GuiClient(), config);
+
     }
 }
---
+++ src/main/java/com/brosintime/rts/View/GuiClient.java
@@ -0,0 +1,11 @@
+package com.brosintime.rts.View;
+
+import com.badlogic.gdx.Game;
+
+public class GuiClient extends Game {
+
+    @Override
+    public void create() {
+
+    }
+}
